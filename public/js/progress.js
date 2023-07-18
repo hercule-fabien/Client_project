0cec4fe7622e88867cf97927b841502fc2de387f
Altynay Muratkyzy <125205125+altynaymm@users.noreply.github.com>
2023-07-18 17:16:28 +0300

    progress bar

---
+++ public/js/progress.js
@@ -0,0 +1,16 @@
+const progressCircle = document.getElementById('progress-circle');
+const countDiv = document.getElementById('progress-count');
+
+(async () => {
+  try {
+    const response = await fetch('/account/progress');
+    const data = await response.json();
+
+    const { progress } = data;
+
+    progressCircle.style.transform = `rotate(${progress * 3.6}deg)`;
+    countDiv.innerHTML = `${progress}%`;
+  } catch (error) {
+    alert('Что-то совсем плохо :((( ', error);
+  }
+})();
--- src/views/Account.jsx
+++ src/views/Account.jsx
@@ -1,10 +1,15 @@
 const React = require('react');
-const Layout = require('../views/Layout');
+const Layout = require('./Layout');
 
-module.exports = function Account(){
-    return(
-        <Layout>
-            
-        </Layout>
-    )
-}
+module.exports = function Account() {
+  return (
+    <Layout>
+      <link rel="stylesheet" href="css/progress.css" />
+      <script defer src="/js/progress.js" />
+      <div id="progress-circle">
+        <div id="progress-count" />
+      </div>
+      <h5></h5>
+    </Layout>
+  );
+};
