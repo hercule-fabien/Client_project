3de40c4cb11c05ce67bf631e286b98bf95d1590f
Vasily Glazkov <vvg.doc@yandex.ru>
2023-07-19 15:48:11 +0200

    cards learned, server & client-side logic

--- public/js/cards.js
+++ public/js/cards.js
@@ -1,7 +1,32 @@
-const cards = document.querySelectorAll('.card');
+document.addEventListener('DOMContentLoaded', () => {
+  const statusElements = document.querySelectorAll('.status');
+  const cards = document.querySelectorAll('.card');
+  const btnLearnedElements = document.querySelectorAll('.btn-learned');
 
-[...cards].forEach((card) => {
-  card.addEventListener('click', () => {
-    card.classList.toggle('is-flipped');
+  [...cards].forEach((card) => {
+    card.addEventListener('click', () => {
+      card.classList.toggle('is-flipped');
+    });
+  });
+
+  btnLearnedElements.forEach((button) => {
+    button.addEventListener('click', async () => {
+      const statusElem = button.parentNode.parentNode.querySelector('.status');
+      const id = Number(button.dataset.cardid);
+      const result = await fetch(`/categories/cards/${id}`, {
+        method: 'PATCH',
+      });
+      const data = await result.json();
+
+      if (data.isLearned) {
+        const cardFaces = button.closest('.card').querySelectorAll('.card__face');
+        cardFaces.forEach((face) => face.classList.add('card__face--learned'));
+        statusElem.innerText = 'Learned';
+      } else {
+        const cardFaces = button.closest('.card').querySelectorAll('.card__face');
+        cardFaces.forEach((face) => face.classList.remove('card__face--learned'));
+        statusElem.innerText = 'Not Learned';
+      }
+    });
   });
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,26 +3,26 @@ const renderTemplate = require('../lib/renderTemplate');
 const { checkUser } = require('../middlewares/common');
 const Welcome = require('../views/Welcome');
 const Cards = require('../views/Cards');
-const { Category, Card } = require('../../db/models');
-const Home = require('../views/Home')
+const { Category, Card, Progress } = require('../../db/models');
+const Home = require('../views/Home');
 
 router.get('/', (req, res) => {
   const { login } = req.session;
   renderTemplate(Welcome, { login }, res);
 });
 
-router.get('/home', async(req,res) => {
+router.get('/home', async (req, res) => {
   const { login } = req.session;
-  const categories = await Category.findAll()
-  renderTemplate(Home, {login, categories}, res)
-})
+  const categories = await Category.findAll();
+  renderTemplate(Home, { login, categories }, res);
+});
 
-router.post('/newCard', async(req,res) => {
-  const {categoryName, question, answer} = req.body
-  const category = await Category.findOne({where: {name:categoryName}})
-  const newCard = await Card.create({categoryId:category.id, question, answer})
-  res.json(newCard)
-})
+router.post('/newCard', async (req, res) => {
+  const { categoryName, question, answer } = req.body;
+  const category = await Category.findOne({ where: { name: categoryName } });
+  const newCard = await Card.create({ categoryId: category.id, question, answer });
+  res.json(newCard);
+});
 
 router.get('/logout', checkUser, (req, res) => {
   req.session.destroy(() => {
@@ -36,8 +36,26 @@ router.get('/categories/:categoryId', async (req, res) => {
   const { categoryId } = req.params;
   try {
     const category = await Category.findByPk(categoryId);
-    const cards = await Card.findAll({ where: { categoryId } });
+    const cards = await Card.findAll(
+      {
+        where: { categoryId },
+        include: Progress,
+      },
+    );
+    console.log('Cards 0 ====> ', cards[0].Progresses[0].dataValues);
     renderTemplate(Cards, { login, category, cards }, res);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.patch('/categories/cards/:id', async (req, res) => {
+  const cardId = req.params.id;
+  try {
+    const progress = await Progress.findOne({ where: { cardId } });
+    progress.isLearned = !progress.isLearned;
+    await progress.save();
+    res.json(progress);
   } catch (error) {
     console.error(error);
   }
