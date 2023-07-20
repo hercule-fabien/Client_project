5c53634b58b8b289eebabb1d5d3613d7a34c948e
Altynay Muratkyzy <125205125+altynaymm@users.noreply.github.com>
2023-07-20 12:14:08 +0300

    solve conflicts

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,33 +1,66 @@
-const router = require('express').Router();
-const bcrypt = require('bcrypt');
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const renderTemplate = require("../lib/renderTemplate");
+const { checkUser } = require("../middlewares/common");
+const Welcome = require("../views/Welcome");
+const Cards = require("../views/Cards");
+const { Category, Card, User, Progress } = require("../../db/models");
+const Home = require("../views/Home");
+const EditPassword = require("../views/EditPassword");
 
-const renderTemplate = require('../lib/renderTemplate');
-const { checkUser } = require('../middlewares/common');
-const Welcome = require('../views/Welcome');
-const Cards = require('../views/Cards');
-
-const {
-  Category, Card, User, Progress,
-} = require('../../db/models');
-const Home = require('../views/Home');
-
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   const { login } = req.session;
   renderTemplate(Welcome, { login, title: 'Домашняя страница' }, res);
 });
 
-router.get('/home', async (req, res) => {
+router.get("/home", async (req, res) => {
   const { login } = req.session;
   const categories = await Category.findAll();
   renderTemplate(Home, { login, categories }, res);
 });
 
-router.post('/newCard', async (req, res) => {
+router.get("/editProfile", (req, res) => {
+  const { login } = req.session;
+  renderTemplate(EditPassword, { login }, res);
+});
+
+router.put("/newPassword", async (req, res) => {
+  const { currentPassword, newPassword, newPasswordValid } = req.body;
+  console.log(req.body, "SMOTRI REQ BODY")
+  const { email } = req.session;
+  //console.log(req.session)
+  const user = await User.findOne({ where: { email } });
+  console.log(user, '<=====USER')
+  //const currentHash = await bcrypt.hash(currentPassword, 10);
+  //console.log()
+  console.log(newPassword,newPasswordValid, user.password, "TUT BIG CONSOLE LOG")
+  const checkPassword = await bcrypt.compare(currentPassword, user.password);
+  if (newPassword === newPasswordValid && checkPassword) {
+    console.log('PROVERKA ZAHODA V IF')
+    const hash = await bcrypt.hash(newPassword, 10);
+    const newPersonalInfo = await User.update(
+      { password: hash },
+      { where: { email } }
+    );
+    console.log(newPersonalInfo, '<==========')
+    //alert('Пароль успешно изменен!')
+    res.json(newPersonalInfo);
+  } else {
+    //alert('Что-то пошло не так, попробуйте еще раз')
+    res.sendStatus(403);
+  }
+});
+
+router.post("/newCard", async (req, res) => {
   const { categoryName, question, answer } = req.body;
   const { email } = req.session;
   const user = await User.findOne({ where: { email } });
   const category = await Category.findOne({ where: { name: categoryName } });
-  const newCard = await Card.create({ categoryId: category.id, question, answer });
+  const newCard = await Card.create({
+    categoryId: category.id,
+    question,
+    answer,
+  });
   await Progress.create({
     isLearned: false,
     userId: user.id,
@@ -36,20 +69,22 @@ router.post('/newCard', async (req, res) => {
   res.json(newCard);
 });
 
-router.get('/logout', checkUser, (req, res) => {
+router.get("/logout", checkUser, (req, res) => {
   req.session.destroy(() => {
-    res.clearCookie('MemorizeCookie');
-    res.redirect('/');
+    res.clearCookie("MemorizeCookie");
+    res.redirect("/");
   });
 });
 
-router.post('/lostpass', async (req, res) => {
+router.post("/lostpass", async (req, res) => {
   function randomPass() {
-    let result = '';
-    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    let result = "";
+    const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
     let counter = 0;
     while (counter < 8) {
-      result += characters.charAt(Math.floor(Math.random() * characters.length));
+      result += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
       counter += 1;
     }
     return result;
@@ -72,37 +107,37 @@ router.post('/lostpass', async (req, res) => {
   }
 });
 
-router.get('/categories/:categoryId', async (req, res) => {
+router.get("/categories/:categoryId", async (req, res) => {
   const { login } = req.session;
   const { categoryId } = req.params;
   const { email } = req.session;
   const user = await User.findOne({ where: { email } });
   try {
     const category = await Category.findByPk(categoryId);
-    const cards = await Card.findAll(
-      {
-        where: { categoryId },
-        include: [
-          {
-            model: Progress,
-            where: { userId: user.id },
-            required: false,
-          },
-        ],
-      },
-    );
+    const cards = await Card.findAll({
+      where: { categoryId },
+      include: [
+        {
+          model: Progress,
+          where: { userId: user.id },
+          required: false,
+        },
+      ],
+    });
     renderTemplate(Cards, { login, category, cards }, res);
   } catch (error) {
     console.error(error);
   }
 });
 
-router.patch('/categories/cards/:id', async (req, res) => {
+router.patch("/categories/cards/:id", async (req, res) => {
   const { email } = req.session;
   const cardId = req.params.id;
   try {
     const user = await User.findOne({ where: { email } });
-    const progress = await Progress.findOne({ where: { cardId, userId: user.id } });
+    const progress = await Progress.findOne({
+      where: { cardId, userId: user.id },
+    });
     if (progress) {
       progress.isLearned = !progress.isLearned;
       await progress.save();
