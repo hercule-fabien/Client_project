33d4da2ac50f17b555322a5cba5af8de99d014b7
Altynay Muratkyzy <125205125+altynaymm@users.noreply.github.com>
2023-07-20 14:50:51 +0300

    merge

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,30 +1,61 @@
-const React = require('react');
-
-const Layout = require('./Layout');
+const React = require("react");
+const Layout = require("./Layout");
 
 module.exports = function Login() {
   return (
     <Layout>
-      <h2 style={{ marginLeft: '180px', marginBottom: '50px', color: '#fbe1a1' }}>Войдите на сайт</h2>
-      <hr />
-      <script defer src="/js/login.js" />
-      <script defer src="/js/lostPass.js" />
-      <script src="https://smtpjs.com/v3/smtp.js"/>
-      <form id="loginForm" style={{ marginTop: '70px' }}>
-        <label htmlFor="exampleInput3" className="form-label">Email</label>
-        <input name="email" type="email" className="form-control" id="email" style={{ width: '80%' }} />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div className="col-md-6">
+          <h2 className="text-center mb-4" style={{ color: "#fbe1a1" }}>
+            Войдите на сайт
+          </h2>
+          <hr />
+          <script defer src="/js/login.js" />
+          <script defer src="/js/lostPass.js" />
+          <script src="https://smtpjs.com/v3/smtp.js" />
+          <form id="loginForm" style={{ marginTop: "30px" }}>
+            <label htmlFor="exampleInput3" className="form-label">
+              Email
+            </label>
+            <input
+              name="email"
+              type="email"
+              className="form-control"
+              id="email"
+              style={{ width: "100%" }}
+            />
 
-        <label htmlFor="exampleInput2" className="form-label">Password</label>
-        <input name="password" type="password" className="form-control" id="exampleInput2" style={{ width: '80%' }} />
-        <button type="submit" className="btn btn-warning">Отправить</button>
-      </form>
-      <button type='button' className='btn btn-outline-dark' id='lostPass'>I forgot my password</button>
-      <hr />
-      <h3 className="logMsg" />
+            <label htmlFor="exampleInput2" className="form-label">
+              Password
+            </label>
+            <input
+              name="password"
+              type="password"
+              className="form-control"
+              id="exampleInput2"
+              style={{ width: "100%" }}
+            />
+            <button type="submit" className="btn btn-warning mt-3">
+              Отправить
+            </button>
+          </form>
+          <button
+            type="button"
+            className="btn btn-outline-dark mt-3"
+            id="lostPass"
+          >
+            I forgot my password
+          </button>
+          <hr />
+          <h3 className="logMsg" />
+        </div>
+      </div>
     </Layout>
   );
 };
-
-
-
-
--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,25 +1,65 @@
-const React = require('react');
-const Layout = require('./Layout');
+const React = require("react");
+const Layout = require("./Layout");
 
 function Register() {
   return (
     <Layout>
-      <h2 style={{marginLeft:"180px", color:"#fbe1a1", opacity:"80%"}}>Зарегистрируйтесь</h2>
-      <hr />
-      <script defer src="/js/reg.js" />
-      <form action="/register" method="POST" id="regForm" style={{marginTop:"70px"}}>
-        <label htmlFor="exampleInput1" className="form-label">Login</label>
-        <input name="login" type="text" className="form-control" id="exampleInput1" style={{width:'80%'}} />
+      <div className="container d-flex justify-content-center align-items-center">
+        <div className="col-md-6">
+          <h2
+            className="text-center mb-4"
+            style={{ color: "#fbe1a1", opacity: "80%" }}
+          >
+            Зарегистрируйтесь
+          </h2>
+          <hr />
+          <script defer src="/js/reg.js" />
+          <form
+            action="/register"
+            method="POST"
+            id="regForm"
+            style={{ marginTop: "20px" }}
+          >
+            <label htmlFor="exampleInput1" className="form-label">
+              Login
+            </label>
+            <input
+              name="login"
+              type="text"
+              className="form-control"
+              id="exampleInput1"
+              style={{ width: "100%" }}
+            />
 
-        <label htmlFor="exampleInput3" className="form-label">Email</label>
-        <input name="email" type="email" className="form-control" id="email" style={{width:'80%'}} />
+            <label htmlFor="exampleInput3" className="form-label">
+              Email
+            </label>
+            <input
+              name="email"
+              type="email"
+              className="form-control"
+              id="email"
+              style={{ width: "100%" }}
+            />
 
-        <label htmlFor="exampleInput2" className="form-label">Password</label>
-        <input name="password" type="password" className="form-control" id="exampleInput2" style={{width:'80%'}}/>
-        <button type="submit" className="btn btn-warning">Отправить</button>
-      </form>
-      <hr />
-      <h3 className="regMsg"></h3>
+            <label htmlFor="exampleInput2" className="form-label">
+              Password
+            </label>
+            <input
+              name="password"
+              type="password"
+              className="form-control"
+              id="exampleInput2"
+              style={{ width: "100%" }}
+            />
+            <button type="submit" className="btn btn-warning mt-3">
+              Отправить
+            </button>
+          </form>
+          <hr />
+          <h3 className="regMsg"></h3>
+        </div>
+      </div>
     </Layout>
   );
 }
